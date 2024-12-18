d856bd458e19ad8a122d748b9dcf43e8cfbe411c
Eva Gandia <evagandiafp@gmail.com>
2024-12-18 06:27:09 +0100

    tuto

---
+++ src/tuto/tuto.java
@@ -0,0 +1,14 @@
+package tuto;
+
+public class tuto {
+
+	public tuto() {
+		// TODO Esbozo de constructor generado automáticamente
+	}
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+	}
+
+}
